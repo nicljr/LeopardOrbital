6fd030c316d78586898c01aaead09e19e535711d
nicljr <99934908+nicljr@users.noreply.github.com>
2022-05-26 22:15:57 +0800

    Add files via upload

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Firebase Config
+export const config = {
+    apiKey: "AIzaSyAU_J5nQ9hgKk-n9WyydJO9fQ0Z5GYqX-s",
+    authDomain: "leopardorbital.firebaseapp.com",
+    projectId: "leopardorbital",
+    storageBucket: "leopardorbital.appspot.com",
+    messagingSenderId: "294983054088",
+    appId: "1:294983054088:web:61cba5e68e21339286f2bd"
+};
